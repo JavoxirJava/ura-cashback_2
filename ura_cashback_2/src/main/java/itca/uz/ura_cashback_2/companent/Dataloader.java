6ea9c2d4bf1b221cd1d241b0eb96e31b405ff082
L.E.G.E.N.D.A <norozod391@gmail.com>
2022-10-08 15:39:51 +0500

    comp tuliq

---
+++ ura_cashback_2/src/main/java/itca/uz/ura_cashback_2/companent/Dataloader.java
@@ -0,0 +1,33 @@
+package itca.uz.ura_cashback_2.companent;
+
+
+import itca.uz.ura_cashback_2.entity.Role;
+import itca.uz.ura_cashback_2.entity.enums.RoleName;
+import itca.uz.ura_cashback_2.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dataloader implements CommandLineRunner {
+    final RoleRepository roleRepository;
+
+
+//    @Value("${spring.sql.init.mode}")
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String initMode;
+
+    public Dataloader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (initMode.equals("create-drop") || initMode.equals("create")) {
+            roleRepository.save(new Role(RoleName.ROLE_USER));
+            roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+            roleRepository.save(new Role(RoleName.ROLE_MODERATOR));
+            roleRepository.save(new Role(RoleName.ROLE_SUPER_ADMIN));
+        }
+    }
+}
--- ura_cashback_2/src/main/java/itca/uz/ura_cashback_2/repository/CompanyRepository.java
+++ ura_cashback_2/src/main/java/itca/uz/ura_cashback_2/repository/CompanyRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    boolean existsCompanyByNameEqualsIgnoreCase(String name);
+    boolean existsCompanyByAttachmentIdEquals(UUID attachment_id);
 }
